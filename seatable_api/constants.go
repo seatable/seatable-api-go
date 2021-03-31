dab75cce0f84fc735f1977f01b92635c4dfe3414
杨赫然 <heran.yang@seafile.com>
2021-03-31 10:27:32 +0800

    Delete column.go

--- seatable_api/constants.go
+++ seatable_api/constants.go
@@ -5,26 +5,6 @@ type ColumnTypes string
 var ROW_FILTER_KEYS []string = []string{"column_name", "filter_predicate", "filter_term", "filter_term_modifier"}
 
 const (
-	NUMBER          ColumnTypes = "number"
-	TEXT            ColumnTypes = "text"
-	CHECKBOX        ColumnTypes = "checkbox"
-	DATE            ColumnTypes = "date"
-	SINGLE_SELECT   ColumnTypes = "single-select"
-	LONG_TEXT       ColumnTypes = "long-text"
-	IMAGE           ColumnTypes = "image"
-	FILE            ColumnTypes = "file"
-	MULTIPLE_SELECT ColumnTypes = "multiple-select"
-	COLLABORATOR    ColumnTypes = "collaborator"
-	LINK            ColumnTypes = "link"
-	FORMULA         ColumnTypes = "formula"
-	CREATOR         ColumnTypes = "creator"
-	CTIME           ColumnTypes = "ctime"
-	LAST_MODIFIER   ColumnTypes = "last-modifier"
-	MTIME           ColumnTypes = "mtime"
-	GEOLOCATION     ColumnTypes = "geolocation"
-	AUTO_NUMBER     ColumnTypes = "auto-number"
-	URL             ColumnTypes = "url"
-
 	RENAME_COLUMN      = "rename_column"
 	RESIZE_COLUMN      = "resize_column"
 	FREEZE_COLUMN      = "freeze_column"
